1936198e295eaba7eea3cbe5f7c6d7bfde18ca5d
Philip <pvu2002@outlook.com>
2022-09-14 01:42:04 -0700

    feat(app): Add metadata for SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,82 @@ export default defineNuxtConfig({
   typescript: {
     shim: false,
   },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@vueuse/nuxt"],
   vue: {
     compilerOptions: {
       // isCustomElement: (tag) => ["apexchart"].includes(tag),
+    },
+  },
+
+  app: {
+    head: {
+      title: "Blockchain Data Analytics Platform",
+      titleTemplate: "%s - COMB Analytics",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "A general purpose analytics dashboard for the COMB protocol. The dashboard provides access to data about the protocol's ongoing transactions, resources and blocks.",
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "Blockchain Data Analytics Platform - COMB Analytics",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content:
+            "A general purpose analytics dashboard for the COMB protocol. The dashboard provides access to data about the protocol's ongoing transactions, resources and blocks.",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content:
+            "https://analytics.comb.financial/_nuxt/waterfall.82d69c55.jpeg",
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: "https://analytics.comb.financial/",
+        },
+        {
+          hid: "og:type",
+          property: "og:type",
+          content: "website",
+        },
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content: "Blockchain Data Analytics Platform - COMB Analytics",
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content:
+            "A general purpose analytics dashboard for the COMB protocol. The dashboard provides access to data about the protocol's ongoing transactions, resources and blocks.",
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content:
+            "https://analytics.comb.financial/_nuxt/waterfall.82d69c55.jpeg",
+        },
+        {
+          hid: "twitter:url",
+          name: "twitter:url",
+          content: "https://analytics.comb.financial/",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,7 @@
 <script setup>
-const chartOptions = {
-  chart: {
-    id: "vuechart-example",
-  },
-  xaxis: {
-    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-  },
-};
-const series = [
-  {
-    name: "series-1",
-    data: [30, 40, 35, 50, 49, 60, 70, 91],
-  },
-];
+useHead({
+  title: "Dashboard",
+});
 </script>
 
 <template>
